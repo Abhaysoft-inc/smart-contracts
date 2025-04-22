01a3be68c034c74607f97d3131665629bf9ac989
Abhaysoft-inc <av385735@gmail.com>
2025-04-22 11:39:17 +0530

    added updateTask and toogleStatus function

--- todoContract.sol
+++ todoContract.sol
@@ -2,7 +2,6 @@
 pragma solidity ^0.8;
 
 contract TodoContract {
-    
 
     struct Task{
         string taskName;
@@ -21,6 +20,18 @@ contract TodoContract {
     function getAllTasks() public  view  returns (Task[] memory){
         return tasks;
     }
+
+    function updateTask(uint _index, string memory _updatedTaskName, bool _updatedIsCompleted) public {
+        require(_index < tasks.length, "Invalid Task Index"); // this means the task doesnot exists
+        tasks[_index].taskName = _updatedTaskName;
+        tasks[_index].isCompleted = _updatedIsCompleted;
+    } // this function is gonna update both the name and the status of the task
+
+    // if we want to toggle the status of isCompleted of the task then we can use this function
+
+    function toogleStatus (uint _index) public {
+    tasks[_index].isCompleted = !tasks[_index].isCompleted;
+    } 
 
    
 
